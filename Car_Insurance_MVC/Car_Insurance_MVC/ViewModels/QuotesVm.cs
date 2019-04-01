c7f893dc6400ed95b239d50f56d5defe5d1976fc
Thamel90 <47161302+Thamel90@users.noreply.github.com>
2019-04-01 14:27:38 -0400

    Add Page 247 Issues

---
+++ Car_Insurance_MVC/Car_Insurance_MVC/ViewModels/QuotesVm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Car_Insurance_MVC.ViewModels
+{
+    public class QuotesVm
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public decimal Quote { get; set; }
+    }
+}
